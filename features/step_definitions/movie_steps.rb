ed84fcf70325a0d275134d6c1e03da9d96163ab8
yaikaew <41978321+yaikaew@users.noreply.github.com>
2023-08-30 13:37:18 +0700

    part4

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -19,7 +19,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  pending "Fill in this step in movie_steps.rb"
+  # pending "Fill in this step in movie_steps.rb"
+  expect(page.body.index(e1) < page.body.index(e2)).to be true
 end
 
 # Make it easier to express checking or unchecking several boxes at once
