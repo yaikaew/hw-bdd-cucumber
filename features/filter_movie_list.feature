f7ab981bdf806ca6fe8ddd4e7b1a7bcaa37bc67c
yaikaew <41978321+yaikaew@users.noreply.github.com>
2023-08-30 13:26:18 +0700

    part3

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -23,14 +23,20 @@ Background: movies have been added to database
   Then 10 seed movies should exist
 
 Scenario: restrict to movies with "PG" or "R" ratings
-  And I check the "PG" checkbox
-  Then complete the rest of of this scenario
+  # When I check the "PG" checkbox
+  # Then complete the rest of of this scenario
   # enter step(s) to check the "PG" and "R" checkboxes
+  When I check the following ratings: PG,R
   # enter step(s) to uncheck all other checkboxes
+  When I uncheck the following ratings: G,PG-13
   # enter step to "submit" the search form on the homepage
+  And I press "Refresh"
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see the following movies: The Terminator,When Harry Met Sally,Amelie,The Incredibles,Raiders of the Lost Ark
   # enter step(s) to ensure that other movies are not visible
+  Then I should not see the following movies: Aladdin,The Help,Chocolat,2001: A Space Odyssey,Chicken Run
 
 Scenario: all ratings selected
   # your steps here
-  Then complete the rest of of this scenario
+  # Then complete the rest of of this scenario
+  Then I should see all the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -30,18 +30,29 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  pending "Fill in this step in movie_steps.rb"
+  # pending "Fill in this step in movie_steps.rb"
+  rating_list.split(",").each do |rating|
+    steps %{Then I #{uncheck}check "ratings_#{rating}"}
+  end
 end
 
 # Part 2, Step 3
 Then /^I should (not )?see the following movies: (.*)$/ do |no, movie_list|
   # Take a look at web_steps.rb Then /^(?:|I )should see "([^"]*)"$/
-  pending "Fill in this step in movie_steps.rb"
+  # pending "Fill in this step in movie_steps.rb"
+  movie_list.split(",").each do |movie|
+    steps %{Then I should #{no}see "#{movie}"}
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  pending "Fill in this step in movie_steps.rb"
+  # pending "Fill in this step in movie_steps.rb"
+  all_movie = Movie.all
+  all_movie.each do |movie|
+    m = movie.title
+    steps %{Then I should see "#{m}"}
+  end
 end
 
 ### Utility Steps Just for this assignment.
